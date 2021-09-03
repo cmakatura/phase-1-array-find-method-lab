a62149d3596a2fa4eba65707009a100a69d5c8c1
christina makatura <cnmak@comcast.net>
2021-09-02 20:18:15 -0500

    commit tree 17b50a82b1ba9afa212bb1ddeefbd4aeddc8ac74

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "1969", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
 
+ /* function superbowlWin(array) {
+    let win = array.find(array => array.result === "W");
+    if (win = "W"){
+        return win.year
+    }
+  }
+  record.find(superbowlWin);
+
+
+  function superbowlWin(array) {
+    let win = array.find(array => array.result === "W");
+    if (win){
+        return record['year'];
+    
+    }
+        
+        
+} 
+*/
+
+function superbowlWin(array) {
+    let win = array.find(array => array.result === "W");
+    if (win){
+        return win.year
+    }
+}
+
+    record.find(superbowlWin);
